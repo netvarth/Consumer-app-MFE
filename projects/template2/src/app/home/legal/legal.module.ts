f13c4dfae858c6e5fbee67f97006ca0e4d5d35da
Thejas <thejas.s@jaldee.com>
2026-03-04 12:52:22 +0530

    Add Terms and Conditions component with module, styling, and routing integration

--- projects/template2/src/app/home/legal/legal.module.ts
+++ projects/template2/src/app/home/legal/legal.module.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TermsconditionModule } from '../../../../../template5/src/app/business/termscondition/termscondition.module';
 import { PrivacyModule } from '../../shared/privacy/privacy.module';
 import { LegalComponent } from './legal.component';
+import { TermsconditionModule } from '../../shared/termscondition/termscondition.module';
 
 const routes: Routes = [
   { path: '', component: LegalComponent }
